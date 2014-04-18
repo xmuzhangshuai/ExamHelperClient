783451f11805b534358b7d3211a02da1977f504d
xmuzhangshuai <1207509737@qq.com>
2014-04-18 19:20:46 +0800

    Signed-off-by: xmuzhangshuai <1207509737@qq.com>

--- ExamHelper/gen/com/handmark/pulltorefresh/library/R.java
+++ ExamHelper/gen/com/handmark/pulltorefresh/library/R.java
@@ -42,11 +42,11 @@ public final class R {
 		public static final int indicator_right_padding = 0x7f080000;
 	}
 	public static final class drawable {
-		public static final int default_ptr_flip = 0x7f020036;
-		public static final int default_ptr_rotate = 0x7f020037;
-		public static final int indicator_arrow = 0x7f02008e;
-		public static final int indicator_bg_bottom = 0x7f02008f;
-		public static final int indicator_bg_top = 0x7f020090;
+		public static final int default_ptr_flip = 0x7f020035;
+		public static final int default_ptr_rotate = 0x7f020036;
+		public static final int indicator_arrow = 0x7f02008d;
+		public static final int indicator_bg_bottom = 0x7f02008e;
+		public static final int indicator_bg_top = 0x7f02008f;
 	}
 	public static final class id {
 		public static final int both = 0x7f070003;
